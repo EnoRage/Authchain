da7b15b48e934d45eb00466d6e68c2f5167aa37a
unknown <krboktv@gmail.com>
2017-11-10 18:25:29 +0300

    поменял require на if

--- contracts.sol
+++ contracts.sol
@@ -10,16 +10,18 @@ contract ImageAuthority {
         autors[msg.sender] = Structures.Author(nameAuthor, email, block.timestamp, msg.sender, block.number); // Добавляем автора
     }
 
-    // ID предыдущей песни
+    // ID предыдущего изображения
     uint256 lastImageId = 0; // Хранится блокчейне
    
-    //Добавляем песню
-    function addImage(string nameImage, string size) public { 
+    //Добавляем изображение
+    function addImage(string nameImage, string size) public returns(string) { 
         bytes32 hashImage = "9999";
-        for (uint256 i = 0; i <= lastImageId; i++) {                                                                                                         // создаем цикл в котором перебираем все id песен 
-            require(keccak256(hashImage) != keccak256(images[i].hashImage));                                                                  // Проверяем на наличие такой картинки                                                                                         // если hash нашей новой песни != hash какой-то песни, то функция останавливается
+        for (uint256 i = 0; i <= lastImageId; i++) {                                                                              // создаем цикл в котором перебираем все id песен 
+            if (keccak256(hashImage) == keccak256(images[i].hashImage)) {                                                         // Проверяем на наличие такой картинки   
+                return "Такая картинка уже есть.";
+            }                                                                             
         }
-        require (autors[msg.sender].addressAuthor == msg.sender);                                                                 // Создан ли автор?                                                                                                                                                // Увеличиваем счетчик на 1    
+        require (autors[msg.sender].addressAuthor == msg.sender);                                                                 // Создан ли автор?
         images[lastImageId] = Structures.Image(nameImage, size, block.timestamp, keccak256(hashImage), msg.sender, block.number); // Добавляем песню
         lastImageId++;  
     }  
@@ -27,11 +29,13 @@ contract ImageAuthority {
 
 //Контракт для изменения автора песни
 contract ChangeSongAuthority is ImageAuthority {
-    function setNewAuthority(string nameImage, address newIdAuthor) public {        // Вводим название песни и адрес нового автора
-        for (uint256 i = 0; i <= lastImageId; i++) {                                // Перебираем весь массив с песнями
-            if (keccak256(nameImage) == keccak256(images[i].nameImage)) {           // Если песня нашлась по названию, то
-                require(images[i].idAuthor == msg.sender);                          // Делаем проверку (изменить может только автор песни)
-                images[i].idAuthor = newIdAuthor;                                   // Присваеваем песне новое авторство
+    function setNewAuthority(string nameImage, address newIdAuthor) public returns(string) {        // Вводим название песни и адрес нового автора
+        for (uint256 i = 0; i <= lastImageId; i++) {                                                // Перебираем весь массив с песнями
+            if (keccak256(nameImage) == keccak256(images[i].nameImage)) {                           // Если песня нашлась по названию, то
+                if (images[i].idAuthor != msg.sender) {                                             // Делаем проверку (изменить может только автор песни)
+                    return "Вы не создали автора.";
+                }
+                images[i].idAuthor = newIdAuthor;                                                   // Присваеваем песне новое авторство
             }
         }
     }
