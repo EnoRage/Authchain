808ac7f088207b51eff74a48178646fe33064a6c
unknown <krboktv@gmail.com>
2017-11-14 22:28:13 +0300

    Добавлена проверка на авторство псевдонима

--- contracts.sol
+++ contracts.sol
@@ -6,35 +6,51 @@ import "./structures.sol";
 
 
 contract ImageAuthority {
-    mapping(address => Structures.Author) public autors; // Этот массив хранит в блокчейне всех авторов
+    mapping(uint256 => Structures.Author) public autors; // Этот массив хранит в блокчейне всех авторов
     mapping(uint256 => Structures.Image)  public images; // Этот массив хранит в блокчейне все песни
+
+    // ID предыдущего автора
+    uint256 authorId = 0; // Хранится блокчейне
 
     // Добавление автора
     function addAuthor(string nameAuthor, string email) public {
-        autors[msg.sender] = Structures.Author(nameAuthor, email, block.timestamp, msg.sender, block.number); // Добавляем автора
+        //Проверяем на авторство псевдоним
+        for (uint256 i = 0; i <= authorId; i++) {
+            require(keccak256(autors[i].nameAuthor) == keccak256(nameAuthor));
+        }
+
+        autors[authorId] = Structures.Author(nameAuthor, email, block.timestamp, msg.sender, block.number); // Добавляем автора
+        authorId++;
     }
 
     // ID предыдущего изображения
-    uint256 lastImageId = 0; // Хранится блокчейне
+    uint256 imageId = 0; // Хранится блокчейне
    
     //Добавляем изображение
     function addImage(string nameImage, string size) public returns(string) { 
         bytes32 hashImage = "9999";
-        for (uint256 i = 0; i <= lastImageId; i++) {                                                                              // создаем цикл в котором перебираем все id песен 
-            // if (keccak256(hashImage) == keccak256(images[i].hashImage)) {                                                      // Проверяем на наличие такой картинки   
+
+        // Проверка на существование изображения
+        for (uint256 i = 0; i <= imageId; i++) {
+            // if (keccak256(hashImage) == keccak256(images[i].hashImage)) {     
             //     return "Такая картинка уже есть.";
             // }                                                                             
         }
-        require (autors[msg.sender].addressAuthor == msg.sender);                                                                 // Создан ли автор?
-        images[lastImageId] = Structures.Image(nameImage, size, block.timestamp, keccak256(hashImage), msg.sender, block.number); // Добавляем песню
-        lastImageId++;  
+
+        // Проверяем на существование автора
+        for (uint256 b = 0; b <= authorId; b++) {
+            require (autors[b].addressAuthor == msg.sender);                                                                            
+        }
+                                                                     
+        images[imageId] = Structures.Image(nameImage, size, block.timestamp, keccak256(hashImage), msg.sender, block.number); // Добавляем песню
+        imageId++;  
     }  
 }
 
 //Контракт для изменения автора изображения
 contract ChangeSongAuthority is ImageAuthority {
     function setNewAuthority(string nameImage, address newIdAuthor) public returns(string) {  // Вводим название песни и адрес нового автора
-        for (uint256 i = 0; i <= lastImageId; i++) {                                          // Перебираем весь массив с песнями
+        for (uint256 i = 0; i <= imageId; i++) {                                              // Перебираем весь массив с песнями
             if (keccak256(nameImage) == keccak256(images[i].nameImage)) {                     // Если изображение нашлось по названию, то
                 require(images[i].idAuthor == msg.sender);                                    // Не даём изменить авторство, если ты не владелец фотки
                 images[i].idAuthor = newIdAuthor;                                             // Если владелец, то изменяем авторство
