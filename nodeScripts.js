23550237b16cf762b8c07f0a8a831ca7c6276dd1
unknown <krboktv@gmail.com>
2017-11-17 23:13:19 +0300

    Добавил node.js

---
+++ nodeScripts.js
@@ -0,0 +1,8 @@
+// var http = require("http");
+// http.createServer(function(request,response){
+     
+//     // var Web3 = require('web3');
+     
+// }).listen(8000, "127.0.0.1",function(){
+//     console.log("Сервер начал прослушивание запросов на порту 8000");
+// });
