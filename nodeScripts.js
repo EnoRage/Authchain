e653dcdd5d206847969f403d7a5e2a0109c86e2d
unknown <krboktv@gmail.com>
2017-11-19 20:32:54 +0300

    crypto

--- nodeScripts.js
+++ nodeScripts.js
@@ -1,8 +1,13 @@
-// var http = require("http");
-// http.createServer(function(request,response){
-     
-//     // var Web3 = require('web3');
-     
-// }).listen(8000, "127.0.0.1",function(){
-//     console.log("Сервер начал прослушивание запросов на порту 8000");
-// });
+// var Web3 = require('web3');
+// var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545", 0, BasicAuthUsername, BasicAuthPassword));
+
+var rs = require('jsrsasign');
+var rsu = require('jsrsasign-util');
+
+var prvKey = rs.KEYUTIL.getKey(НУЖЕН PEM ФАЙЛ);
+var sig = new a.Signature({alg: 'SHA1withRSA'});
+sig.init(prvKey);
+sig.updateString('aaa');
+var sigVal = sig.sign();
+
+//Остановился на том, что мне понадобился PEM файл, который создаётся с помощью OpenSSl))))
